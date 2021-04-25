75e7272058e15f0aa3436d0c8ffd1eefd72d9160
Dmytro Nazarchuk <nazarchuk.dmitrij@gmail.com>
2021-04-25 12:55:14 +0200

    Response caching added in rate.js

--- src/store/modules/rate.js
+++ src/store/modules/rate.js
@@ -1,16 +1,34 @@
 export default {
   actions: {
     async fetchRates({ commit }, currency = "EUR") {
-      const res = await fetch(
-        "https://api.ratesapi.io/api/latest?base=" + currency
-      );
-      const rates = await res.json();
+      const cachedRates = JSON.parse(localStorage.getItem(currency));
+      const currentTime = Date.now();
+      let response;
+      if (cachedRates && cachedRates.expires > currentTime) {
+        response = cachedRates.data;
+      } else {
+        const data = await fetch(
+          "https://api.ratesapi.io/api/latest?base=" + currency
+        );
+        response = await data.json();
 
-      commit("UPDATE_RATES", rates);
+        // Caching response in localStorage for one hour
+        const expireTime = 60 * 60 * 1000;
+        let expireDate = currentTime + expireTime;
+        let storeRate = {
+          currency: currency,
+          data: response,
+          expires: expireDate,
+        };
+
+        localStorage.setItem(currency, JSON.stringify(storeRate));
+      }
+
+      commit("SET_RATES", response);
     },
   },
   mutations: {
-    UPDATE_RATES(state, rates) {
+    SET_RATES(state, rates) {
       state.rates = rates;
     },
   },
