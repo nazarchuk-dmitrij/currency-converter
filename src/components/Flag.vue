499a266c88b6907d9de85c24e2a0b11b2ded6cc3
Dmytro Nazarchuk <nazarchuk.dmitrij@gmail.com>
2021-05-23 20:36:44 +0200

    flag classnames changed

--- src/components/Flag.vue
+++ src/components/Flag.vue
@@ -11,7 +11,7 @@ export default {
   },
   computed: {
     flagClass() {
-      return "flag-" + this.currency.slice(0, -1);
+      return "flag--" + this.currency.slice(0, -1);
     },
   },
 };
@@ -35,107 +35,107 @@ export default {
   height: 29px;
   border-radius: 5px;
 }
-.flag-CH {
+.flag--CH {
   background-position-x: -7px;
   width: 28px;
 }
-.flag-ZA {
+.flag--ZA {
   background-position-y: 0px;
 }
-.flag-BR {
+.flag--BR {
   background-position-y: -29px;
 }
-.flag-CA {
+.flag--CA {
   background-position-y: -58px;
 }
-.flag-MX {
+.flag--MX {
   background-position-y: -87px;
 }
-.flag-US {
+.flag--US {
   background-position-y: -116px;
 }
-.flag-CN {
+.flag--CN {
   background-position-y: -145px;
 }
-.flag-HK {
+.flag--HK {
   background-position-y: -174px;
 }
-.flag-IN {
+.flag--IN {
   background-position-y: -203px;
 }
-.flag-ID {
+.flag--ID {
   background-position-y: -232px;
 }
-.flag-JP {
+.flag--JP {
   background-position-y: -261px;
 }
-.flag-MY {
+.flag--MY {
   background-position-y: -290px;
 }
-.flag-PH {
+.flag--PH {
   background-position-y: -319px;
 }
-.flag-SG {
+.flag--SG {
   background-position-y: -348px;
 }
-.flag-KR {
+.flag--KR {
   background-position-y: -377px;
 }
-.flag-TH {
+.flag--TH {
   background-position-y: -406px;
 }
-.flag-BG {
+.flag--BG {
   background-position-y: -435px;
 }
-.flag-HR {
+.flag--HR {
   background-position-y: -464px;
 }
-.flag-CZ {
+.flag--CZ {
   background-position-y: -493px;
 }
-.flag-DK {
+.flag--DK {
   background-position-y: -522px;
 }
-.flag-HU {
+.flag--HU {
   background-position-y: -551px;
 }
-.flag-IS {
+.flag--IS {
   background-position-y: -580px;
 }
-.flag-NO {
+.flag--NO {
   background-position-y: -609px;
 }
-.flag-PL {
+.flag--PL {
   background-position-y: -638px;
 }
-.flag-RO {
+.flag--RO {
   background-position-y: -667px;
 }
-.flag-RU {
+.flag--RU {
   background-position-y: -696px;
 }
-.flag-SE {
+.flag--SE {
   background-position-y: -725px;
 }
-.flag-CH {
+.flag--CH {
   background-position-y: -754px;
 }
-.flag-TR {
+.flag--TR {
   background-position-y: -783px;
 }
-.flag-GB {
+.flag--GB {
   background-position-y: -812px;
 }
-.flag-IL {
+.flag--IL {
   background-position-y: -841px;
 }
-.flag-AU {
+.flag--AU {
   background-position-y: -870px;
 }
-.flag-NZ {
+.flag--NZ {
   background-position-y: -899px;
 }
-.flag-EU {
+.flag--EU {
   background-position-y: -928px;
 }
 </style>
